9de6dc91fa63c7e1d4407979d967e6fc0c3a2f1c
Esha Shah <eshah03@fb.com>
2022-06-07 15:17:36 -0700

    Commit

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -1,11 +1,18 @@
  package com.codepath.apps.restclienttemplate;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
@@ -14,6 +21,7 @@ import com.github.scribejava.apis.TwitterApi;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,6 +34,7 @@ import okhttp3.Headers;
      RecyclerView rvTweets;
      List<Tweet> tweets;
      TweetsAdapter adapter;
+     private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +42,13 @@ import okhttp3.Headers;
         setContentView(R.layout.activity_timeline);
 
         client = TwitterApp.getRestClient(this);
+        button = (Button) findViewById(R.id.logoutbtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onLogoutButton();
+            }
+        });
 
         //Find the recycler view
         rvTweets = findViewById(R.id.rvTweets);
@@ -42,6 +58,32 @@ import okhttp3.Headers;
         rvTweets.setAdapter(adapter);
         populateHomeTimeLine();
     }
+
+     void onLogoutButton() {
+         // forget who's logged in
+         TwitterApp.getRestClient(this).clearAccessToken();
+
+         // navigate backwards to Login screen
+         Intent i = new Intent(this, LoginActivity.class);
+         i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // this makes sure the Back button won't work
+         i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // same as above
+         startActivity(i);
+     }
+
+     @Override
+     public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+         return true;
+     }
+
+     @Override
+     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.compose){
+            Toast.makeText(this, "Compose!", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+         return super.onOptionsItemSelected(item);
+     }
 
      private void populateHomeTimeLine() {
          client.getHomeTimeline(new JsonHttpResponseHandler() {
